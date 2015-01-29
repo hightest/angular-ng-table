d69f22cf3c2fa75000484fce3614b7231dcbf2df
Nowicki <adrian.nowicki@outlook.com>
2015-01-29 09:50:45 +0100

    checked

--- demo/demo.js
+++ demo/demo.js
@@ -2,21 +2,21 @@ angular.module('demo', ['ht.table']).controller('DemoCtrl', function ($scope, $f
     $scope.data = [
         {id: 1, name: "Moroni", age: 50},
         {id: 1, name: "Tiancum", age: 43},
-        {id: 3, name: "Jacob", age: 27},
-        {id: 4, name: "Nephi", age: 29},
-        {id: 5, name: "Enos", age: 34},
-        {id: 6, name: "Tiancum", age: 43},
-        {id: 7, name: "Jacob", age: 27},
-        {id: 8, name: "Nephi", age: 29},
-        {id: 9, name: "Enos", age: 33},
-        {id: 10, name: "Tiancum", age: 43},
-        {id: 11, name: "Jacob", age: 27},
-        {id: 12, name: "Nephi", age: 29},
-        {id: 13, name: "Enos", age: 34},
-        {id: 14, name: "Tiancum", age: 43},
-        {id: 15, name: "Jacob", age: 27},
-        {id: 16, name: "Nephi", age: 29},
-        {id: 17, name: "Enos", age: 34}
+        {id: 1, name: "Jacob", age: 27},
+        {id: 1, name: "Nephi", age: 29},
+        {id: 1, name: "Enos", age: 34},
+        {id: 1, name: "Tiancum", age: 43},
+        {id: 1, name: "Jacob", age: 27},
+        {id: 1, name: "Nephi", age: 29},
+        {id: 1, name: "Enos", age: 33},
+        {id: 1, name: "Tiancum", age: 43},
+        {id: 1, name: "Jacob", age: 27},
+        {id: 1, name: "Nephi", age: 29},
+        {id: 1, name: "Enos", age: 34},
+        {id: 1, name: "Tiancum", age: 43},
+        {id: 1, name: "Jacob", age: 27},
+        {id: 1, name: "Nephi", age: 29},
+        {id: 1, name: "Enos", age: 34}
     ];
 
     $scope.settings = {
